5ed48dc444fe899f6a09a3f49d4e07be81c5e1fb
Shah jalal <jalal.chishti122448@gmail.com>
2025-04-04 20:41:17 +0500

    JS display properties(innerHTML,innerTEXT,Window.write,more

---
+++ script2.js
@@ -0,0 +1,10 @@
+document.getElementById("demo").innerHTML = "<h2>Shah Jalal</h2>";
+
+document.getElementById("demo2").innerText = "Sweet_Boy";
+
+document.write(5 + 6);
+
+window.alert(6 + 9);
+//alert(6 + 9); same because window keyword is optional
+
+console.log(9 + 9);
